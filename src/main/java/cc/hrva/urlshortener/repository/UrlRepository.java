7a1be6ad95724c23487e10d07a0a13018fdae4a6
Karlo Hrvačić <karlo.hrvacic@greenbytehub.hr>
2025-10-21 15:06:02 +0200

    remove caching annotations from UrlRepository

--- src/main/java/cc/hrva/urlshortener/repository/UrlRepository.java
+++ src/main/java/cc/hrva/urlshortener/repository/UrlRepository.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import cc.hrva.urlshortener.model.Url;
 import cc.hrva.urlshortener.model.User;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,11 +14,7 @@ public interface UrlRepository extends JpaRepository<Url, Long> {
     Optional<List<Url>> findAllByOwner(User owner);
     boolean existsUrlByLongUrlAndActiveTrue(String longUrl);
     Optional<Url> findByLongUrlAndActiveTrue(String longUrl);
-
-    @Cacheable(value = "url-exists")
     boolean existsUrlByShortUrlAndActiveTrue(String shortUrl);
-
-    @Cacheable(value = "url")
     Optional<Url> findByShortUrlAndActiveTrue(String shortUrl);
     boolean existsUrlByLongUrlAndActiveTrueAndOwnerIsNull(String longUrl);
     List<Url> findByExpirationDateLessThanEqualAndActiveTrue(LocalDateTime expirationDate);
